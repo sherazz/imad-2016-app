e7c06926227a16ae23094796cc06f2124ed29388
SHERAZ <sheraz_95@yahoo.com>
2016-09-29 14:21:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var counter=0;
+app.get('/counter',function(req,res)
+{
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,13 +25,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
-var counter=0;
-app.get('/counter',function(req,res)
-{
-   counter=counter+1;
-   res.send(counter.toString());
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
